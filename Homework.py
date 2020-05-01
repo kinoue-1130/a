14989ba50956351e16669b2899365b9f99138dd2
kinoue-1130 <kiwami.inoue1130@gmail.com>
2020-05-02 01:43:37 +0900

    Homework.py

--- Homework.py
+++ Homework.py
@@ -1,24 +1,33 @@
 # -*- coding: utf-8
 
-i = list(map(int,input().split()))#N,M取得
+import sys
 
-if 1 <= i[0] and i[0] <= 1000000 and 1 <= i[1] and i[1] <+ 1000:
-    i_2 = list(map(int,input().split()))#A1-AM取得
+def asobi(a,b):
 
-    sum = 0
+    nokori = a-b
 
-    for x in range(len(i_2)):
-        if 1 <= i_2[x] and i_2[x] <= 10000:
-            sum = sum + i_2[x]#課題をする日数
-        else:
-            print('Aiは1から10000で入力してください')
-            break
-    
-    vac = i[0] - sum#休暇（休み全日ー課題の日数）
-
-    if 0 <= vac:
-        print(vac)
+    if 0 <= nokori:
+        nokori = nokori
     else:
-        print(-1)#宿題が終わらなかった
-else:
-    print('Nは1から1000000、またはMは1から10000で入力してください')
+        nokori = -1
+
+    return nokori
+
+if __name__ == "__main__":
+
+    i = list(map(int,input().split()))#N,M取得
+
+    if 1 <= i[0] and i[0] <= 1000000 and 1 <= i[1] and i[1] <+ 1000:
+
+        i_2 = list(map(int,input().split()))#A1-AM取得
+
+        for x in range(len(i_2)):
+            if i_2[x] <= 1 or 10000 <= i_2[x]:
+                print('Aiは1から10000で入力してください')
+                sys.exit()
+
+        max_asobi = asobi(i[0],sum(i_2))
+        print(max_asobi)
+
+    else:
+        print('Nは1から1000000、またはMは1から10000で入力してください')
